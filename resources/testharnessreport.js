01a3115c076a3ad0c84849dbbf77a6e3d199c56f
Pierre Tachoire <pierre@lightpanda.io>
2026-02-13 10:28:12 +0100

    override default testharnessreport
    For simpler usage with Lightpanda WPT test runner, we override the
    default testharnessreport.

--- resources/testharnessreport.js
+++ resources/testharnessreport.js
@@ -1,32 +1,36 @@
-/* global add_completion_callback */
-/* global setup */
-
 /*
- * This file is intended for vendors to implement code needed to integrate
- * testharness.js tests with their own test systems.
- *
- * Typically test system integration will attach callbacks when each test has
- * run, using add_result_callback(callback(test)), or when the whole test file
- * has completed, using
- * add_completion_callback(callback(tests, harness_status)).
- *
- * For more documentation about the callback functions and the
- * parameters they are called with see testharness.js
+ * Prepare a report object containing all tests results.
+ * https://wpt-docs.readthedocs.io/en/latest/_writing-tests/testharness-api.html#callback-api
  */
+var report = {
+  status: "",
+  log: "no test suite completion|Fail|The test never reaches the completion callback.",
+  cases: {},
+};
 
-/* If the parent window has a testharness_properties object,
- * we use this to provide the test settings. This is used by the
- * default in-browser runner to configure the timeout and the
- * rendering of results
- */
-try {
-    if (window.opener && "testharness_properties" in window.opener) {
-        /* If we pass the testharness_properties object as-is here without
-         * JSON stringifying and reparsing it, IE fails & emits the message
-         * "Could not complete the operation due to error 80700019".
-         */
-        setup(JSON.parse(JSON.stringify(window.opener.testharness_properties)));
-    }
-} catch (e) {
+function format(test) {
+  var log = test.name+"|"+test.format_status();
+  if (test.message != null) {
+    log +=  "|"+test.message.replaceAll("\n"," ");
+  }
+
+  return log;
 }
-// vim: set expandtab shiftwidth=4 tabstop=4:
+
+function update() {
+  var log = "";
+  Object.keys(report.cases).forEach((k, i) => {
+    log += report.cases[k] + "\n";
+  });
+  report.log = log;
+}
+
+add_test_state_callback(function (test) {
+  report.cases[test.name] = format(test);
+  update();
+});
+
+add_result_callback(function (test) {
+  report.cases[test.name] = format(test);
+  update();
+});
